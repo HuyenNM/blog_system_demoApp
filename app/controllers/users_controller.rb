26783e3f7dfaa85fd2ec6ef279a09f87c5a7d334
framgia <framgia@ubuntu.(none)>
2014-04-02 16:35:58 -0400

    add user function

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,31 @@
 class UsersController < ApplicationController
+
+  def index
+    @users = User.paginate(page: params[:page])
+  end
+
+    #destroy
+    def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted."
+    redirect_to users_url
+  end
+  #end destroy
+    def edit
+    @user = User.find(params[:id])
+  end
+
+    def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+
+  
   def new
     @user = User.new
   end
